0a0503165a225319cfcfcb5553b5ce49cccd043a
lbjcena <lbjcena@gmail.com>
2025-08-26 22:04:48 +0200

    ArticleList improvements - box shadow added, scale on hover effect, rewritten to use v-for, using data from an array instead of hardcoded values

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,43 +1,41 @@
+<script setup lang="ts">
+
+// In a real project this would probably be a fetch from a database, but this is a mock-up, so a simple array will do
+const articleList: ArticleItemSimple[] = [
+  {
+    id: 1,
+    img: "/image-retro-pcs.jpg",
+    title: "Reviving Retro PCs"
+  },
+  {
+    id: 2,
+    img: "/image-top-laptops.jpg",
+    title: "Top 10 Laptops of 2025"
+  },
+  {
+    id: 3,
+    img: "/image-gaming-growth.jpg",
+    title: "The Growth of Gaming"
+  }
+
+]
+
+</script>
+
 <template>
-  <section aria-labelledby="hotspot-heading" class="article_list">
-    <h2 id="hotspot-heading" class="sr-only">Hotspot articles</h2>
-    <ul class="flex flex-col desktop:flex-row gap-4" role="list">
-      <li>
-        <a href="#" class="article flex gap-3" aria-label="Read more about Reviving Retro PCs">
-          <img src="/image-retro-pcs.jpg" alt="Old retro computer setup" class="w-[150px]">
+  <section aria-labelledby="hotspot-heading" class="article_list flex flex-col gap-8 h-fit">
+    <h2 id="hotspot-heading" class="font-bold text-primary-red">Today's picks</h2>
+    <ul class="flex flex-col desktop:flex-row gap-6" role="list">
+      <li v-for="i in articleList" class="overflow-hidden shadow-base rounded-sm h-fit hover:scale-105 duration-250">
+        <a href="#" class="article flex gap-3 h-fit" aria-label="Read more about Reviving Retro PCs">
+          <img :src="i.img" alt="Old retro computer setup" class="min-w-[200px]">
           <div class="text flex flex-col gap-2">
-            <span class="h3 text-primary-red font-medium">01</span>
-            <div class="text_content flex flex-col h-full justify-between">
-              <h3 class="font-extrabold">Reviving Retro PCs</h3>
-              <p class="desktop:text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, qui.
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
-
-      <li>
-        <a href="#" class="article flex gap-3" aria-label="Read more about Top 10 Laptops of 2025">
-          <img src="/image-top-laptops.jpg" alt="Modern laptop on a desk" class="w-[150px]">
-          <div class="text flex flex-col gap-2">
-            <span class="h3 text-primary-red font-medium">02</span>
-            <div class="text_content flex flex-col h-full justify-between">
-              <h3 class="font-extrabold">Top 10 Laptops of 2025</h3>
-              <p class="desktop:text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, qui.
-              </p>
-            </div>
-          </div>
-        </a>
-      </li>
-
-      <li>
-        <a href="#" class="article flex gap-3" aria-label="Read more about The Growth of Gaming">
-          <img src="/image-gaming-growth.jpg" alt="Person playing a video game" class="w-[150px]">
-          <div class="text flex flex-col gap-2">
-            <span class="h3 text-primary-red font-medium">03</span>
-            <div class="text_content flex flex-col h-full justify-between">
-              <h3 class="font-extrabold">The Growth of Gaming</h3>
-              <p class="desktop:text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, qui.
+            <span class="h3 text-primary-red font-medium">0{{ i.id }}</span>
+            <div class="text_content flex flex-col justify-around h-full">
+              <h3 class="font-extrabold">{{ i.title }}</h3>
+              <p class="desktop:text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aspernatur,
+                qui.
               </p>
             </div>
           </div>
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -11,3 +11,10 @@ interface ArticleBlock {
   blockTitle: string
   blockItems: ArticleItem[]
 }
+
+// used in the ArticleList component - it's the little 'photo + title + mini-description one
+interface ArticleItemSimple {
+  id: string | number;
+  img: string;
+  title: string;
+}
